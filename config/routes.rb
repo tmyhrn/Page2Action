817a5c4d130113e062fd1fbd6bc98a455c7a40db
Hirano Tomoya <ht.tomoya0605@gmail.com>
2023-07-05 06:25:17 +0000

    [Add]deviseインストール

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
+  
+  #管理者用
+  devise_for :admins, skip: [:passwords], controllers: {
+    registrations: "admin/registrations",
+    sessions: "admin/sessions"
+  }
+  
+  #会員用
+  devise_for :customers, skip: [:passwords], controllers: {
+    registrations: "public/registrations",
+    sessions: "public/sessions"
+  }
+  
+  devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
