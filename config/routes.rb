82254ca35a3615eb84392a09fc1134189badeb60
Hirano Tomoya <ht.tomoya0605@gmail.com>
2023-07-05 07:06:07 +0000

    [Update]会員・管理者のログイン実装＋デザイン変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   
+  namespace :public do
+    get 'homes/top'
+    get 'homes/about'
+  end
   #管理者用
-  devise_for :admins, skip: [:passwords], controllers: {
+  devise_for :admin, skip: [:passwords], controllers: {
     registrations: "admin/registrations",
     sessions: "admin/sessions"
   }
