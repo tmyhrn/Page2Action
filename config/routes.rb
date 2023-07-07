95a1a3a8a6781d57ecee6a69199b72e81bb62389
Hirano Tomoya <ht.tomoya0605@gmail.com>
2023-07-07 11:17:35 +0000

    [Add]管理者ページにて会員の一覧・詳細を表示

--- config/routes.rb
+++ config/routes.rb
@@ -21,6 +21,10 @@ Rails.application.routes.draw do
     sessions: "admin/sessions"
   }
   
+  namespace :admin do
+    resources :customers, only: [:index, :show, :edit, :update]
+  end
+  
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
