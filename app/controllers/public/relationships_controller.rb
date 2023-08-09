61ff49145ab47d52a7f007f902db1ddcb26ca70e
Hirano Tomoya <ht.tomoya0605@gmail.com>
2023-08-09 06:30:10 +0000

    [Update]フォロー機能にフラッシュメッセージを追加

--- app/controllers/public/relationships_controller.rb
+++ app/controllers/public/relationships_controller.rb
@@ -3,13 +3,15 @@ class Public::RelationshipsController < ApplicationController
   # フォローするとき
   def create
     current_customer.follow(params[:customer_id])
+    flash[:success] = "フォローしました！"
     redirect_to request.referer
   end
   
   # フォロー外すとき
   def destroy
     current_customer.unfollow(params[:customer_id])
-    redirect_to request.referer  
+    flash[:success] = "フォローを解除しました。"
+    redirect_to request.referer
   end
   
   # フォローしている会員を取得・表示
